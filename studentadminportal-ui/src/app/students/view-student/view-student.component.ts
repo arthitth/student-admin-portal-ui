35cbcd49be16f3194fe4287b19bc2358cc83c3a0
arthitth <the.arthit@gmail.com>
2022-09-09 20:31:46 +0700

    End of section 7

--- studentadminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -32,6 +32,8 @@ export class ViewStudentComponent implements OnInit {
       postalAddress: ''
     }
   };
+  isNewStudent = false;
+  header = '';
   genderList: Gender[] = [];
 
   constructor(private readonly studentService: StudentService,
@@ -46,13 +48,25 @@ export class ViewStudentComponent implements OnInit {
         this.studentId = params.get('id');
 
         if (this.studentId) {
-          this.studentService.getStudent(this.studentId)
-            .subscribe(
-              (successResponse) => {
-                // console.log(successResponse)
-                this.student = successResponse;
-              }
-            );
+          if (this.studentId.toLowerCase() === 'Add'.toLowerCase()) {
+            // -> new Student Functionality
+            this.isNewStudent = true;
+            this.header = 'Add New Student';
+          } else {
+              // -> Existing Student Functionality
+              this.isNewStudent = false;
+              this.header = 'Edit Student';
+
+              this.studentService.getStudent(this.studentId)
+              .subscribe(
+                (successResponse) => {
+                  // console.log(successResponse)
+                  this.student = successResponse;
+                }
+              );
+
+          };
+
         }
         this.genderService.getGenderList()
         .subscribe(
@@ -85,7 +99,7 @@ export class ViewStudentComponent implements OnInit {
     this.studentService.deleteStudent(this.student.id)
       .subscribe(
         (successResponse) => {
-          console.log(this.student)
+          // console.log(this.student)
           this.snackbar.open('Student deleted successfully', undefined, {
             duration: 2000
           });
@@ -98,5 +112,26 @@ export class ViewStudentComponent implements OnInit {
           // Log
         }
       );
+  }
+
+  onAdd(): void {
+    this.studentService.addStudent(this.student)
+      .subscribe(
+        (successResponse) => {
+          // console.log(this.student)
+          this.snackbar.open('Student added successfully', undefined, {
+            duration: 2000
+          });
+
+          setTimeout(() => {
+            this.router.navigateByUrl(`students/${successResponse.id}`);
+          }, 2000);
+
+        },
+        (errorResponse) => {
+          // Log
+        }
+      );
+
   }
 }
